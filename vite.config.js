c29a3060af56d44debb87f46852044a9479d473f
sandy <sandywen820s@gmail.com>
2025-12-26 16:39:34 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HomeView/', // 例如 '/my-vue-portfolio/'
+  base: '/sandy-studio/', // 例如 '/my-vue-portfolio/'
   plugins: [
     vue(),
     vueDevTools(),
